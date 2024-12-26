ad9d166b1301f7d22d7a96c3b31b83979b868710
KangBaekGwa <ksu9801@gmail.com>
2024-12-26 18:07:02 +0900

    feat : 기본 설정 진행 - test, prod, dev 환경 분리 - 의존성 추가

---
+++ backend/src/test/java/baekgwa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package baekgwa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
