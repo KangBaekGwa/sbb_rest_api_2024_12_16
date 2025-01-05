76fa3dcf8c1a2118ecba98c6e4b52904e678f002
KangBaekGwa <ksu9801@gmail.com>
2025-01-05 18:31:21 +0900

    fix : response 응답 형식, code 부분 동적으로 변할 수 있도록 수정

--- backend/src/main/java/baekgwa/backend/global/response/SuccessCode.java
+++ backend/src/main/java/baekgwa/backend/global/response/SuccessCode.java
@@ -9,21 +9,22 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     //User Domain
-    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입 성공"),
-    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+    SIGNUP_SUCCESS(HttpStatus.CREATED, Boolean.TRUE, 201, "회원가입 성공"),
+    LOGIN_SUCCESS(HttpStatus.OK, Boolean.TRUE, 200, "로그인 성공"),
 
     //Board Domain
-    FIND_BOARD_LIST_SUCCESS(HttpStatus.OK, "게시판 조회 성공"),
-    CREATE_NEW_QUESTION_SUCCESS(HttpStatus.CREATED, "신규 질문 등록 성공"),
-    FIND_QUESTION_DETAIL_SUCCESS(HttpStatus.OK, "질문 상세 조회 성공"),
+    FIND_BOARD_LIST_SUCCESS(HttpStatus.OK, Boolean.TRUE, 200, "게시판 조회 성공"),
+    CREATE_NEW_QUESTION_SUCCESS(HttpStatus.CREATED, Boolean.TRUE, 201, "신규 질문 등록 성공"),
+    FIND_QUESTION_DETAIL_SUCCESS(HttpStatus.OK, Boolean.TRUE, 200, "질문 상세 조회 성공"),
+    CREATE_NEW_ANSWER_SUCCESS(HttpStatus.CREATED, Boolean.TRUE, 201, "신규 질문 등록 성공"),
 
     //공통
-    SUCCESS(HttpStatus.OK, "요청 응답 성공"),
-    REISSUE_TOKEN_SUCCESS(HttpStatus.OK, "Token 재발급 성공"),
+    SUCCESS(HttpStatus.OK, Boolean.TRUE, 200, "요청 응답 성공"),
+    REISSUE_TOKEN_SUCCESS(HttpStatus.OK, Boolean.TRUE, 200, "Token 재발급 성공"),
     ;
 
     private final HttpStatus httpStatus;
-    private final Boolean isSuccess = true;
-    private final int code = 200;
+    private final Boolean isSuccess;
+    private final int code;
     private final String message;
 }
